5b4ea4aeae9963a7f56014f225bb516705f3e38d
cuzoaru90@gmail.com <cuzoaru90@gmail.com>
2015-12-14 09:27:36 -0600

    Defined custom directive for seek bar

---
+++ app/scripts/directives/seekBar.js
@@ -0,0 +1,14 @@
+(function() {
+  function seekBar() {
+    return {
+      templateUrl: '/templates/directives/seek_bar.html',
+      replace: true,
+      restrict: 'E'
+    };
+
+  }
+ 
+  angular
+    .module('blocJams')
+    .directive('seekBar', seekBar);
+})();
