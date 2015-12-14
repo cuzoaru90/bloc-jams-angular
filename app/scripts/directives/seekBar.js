946824e8a6973ac3b7be350565a627c979b0cbac
cuzoaru90@gmail.com <cuzoaru90@gmail.com>
2015-12-14 10:00:55 -0600

    Seek bar length changes when clicked

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -1,9 +1,62 @@
 (function() {
   function seekBar() {
+    
+    /**
+    * @function calculatePercent
+    * @desc Calculates percentage of seek bar length
+    * @params {Object} seekBar, {Object} event
+    */
+    var calculatePercent = function(seekBar, event) {
+      var offsetX = event.pageX - seekBar.offset().left;
+      var seekBarWidth = seekBar.width();
+      var offsetXPercent = offsetX / seekBarWidth;
+      offsetXPercent = Math.max(0, offsetXPercent);
+      offsetXPercent = Math.min(1, offsetXPercent);
+      return offsetXPercent;
+    };
+
     return {
       templateUrl: '/templates/directives/seek_bar.html',
       replace: true,
-      restrict: 'E'
+      restrict: 'E',
+      scope: { },
+      //updates DOM
+      link: function(scope, element, attributes) {
+      // bulk of directive logic is placed here and is returned
+      scope.value = 0;
+      scope.max = 100;
+
+      var seekBar = $(element);
+      
+      /**
+      * @function percentString
+      * @desc Converts percentage number to string type
+      */
+      var percentString = function () {
+        var value = scope.value;
+        var max = scope.max;
+        var percent = value / max * 100;
+        return percent + "%";
+      };
+      
+      /**
+      * @function fillStyle
+      * @desc Returns width of bar based on percent
+      */
+      scope.fillStyle = function() {
+        return {width: percentString()};
+      };
+      
+      /**
+      * @function onClickSeekBar
+      * @desc Updates seek bar value based on bar width and location of click
+      */
+      scope.onClickSeekBar = function(event) {
+        var percent = calculatePercent(seekBar, event);
+        scope.value = percent * scope.max;
+      };
+
+      } // end of link method
     };
 
   }
